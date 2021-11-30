09ad1b0f754c36ac2611cbc301941395fc53af97
Nazar-kob <kobrinnazar05@mail.com>
2021-11-30 20:17:15 +0200

    kyu 5 30.11.2021

---
+++ help.py
@@ -0,0 +1,5 @@
+#  f = lambda x: True if x == 1 else False
+#  print(list(map(f, [1, 2, 3])))
+#  s = s[s.index(x):] + s[s.index(x)+1:]
+# for item in range(len(list_number)-1):
+#         if list_number[item+1] > list_number[item]:
